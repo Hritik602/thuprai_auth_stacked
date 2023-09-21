68bdeb0fd16644900f989a36c39c2945b7da8267
hritik602 <hritikrajkarn@gmail.com>
2023-09-21 10:54:11 +0545

    feat: add common strings

--- lib/ui/common/app_strings.dart
+++ lib/ui/common/app_strings.dart
@@ -1,3 +1,10 @@
 const String ksHomeBottomSheetTitle = 'Build Great Apps!';
 const String ksHomeBottomSheetDescription =
     'Stacked is built to help you build better apps. Give us a chance and we\'ll prove it to you. Check out stacked.filledstacks.com to learn more';
+
+//Login & Sign up
+const String emailHintText = "Enter email";
+const String passwordText = "Enter Password";
+
+//Button Name
+const String loginButtonName = "Login";
